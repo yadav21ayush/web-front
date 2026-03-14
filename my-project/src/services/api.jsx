b00f631851c1efe3dc152b5ba8d33909b4a8c706
Ayush yadav <153013262+yadav21ayush@users.noreply.github.com>
2026-03-15 02:29:55 +0530

    Delete my-project/src directory

--- my-project/src/services/api.jsx
+++
@@ -1,7 +0,0 @@
-
-const api = {
-  get: (url) => window.axios.get(`http://localhost:3001${url}`),
-  post: (url, data) => window.axios.post(`http://localhost:3001${url}`, data),
-};
-
-export default api;
